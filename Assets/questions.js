70f6f7b2b0f5bc38badf203569d2ff644882eee0
Antonio Gonzalez <goantonio31@outlook.com>
2020-11-04 11:57:18 -0800

    first commit

---
+++ Assets/questions.js
@@ -0,0 +1,42 @@
+var jsQuestions = [
+  {
+    title: " Which symbol is used for comments in Javascript?",
+    choices: ["//", "*/", "\\", "~"],
+    answer: "//",
+  },
+  {
+    title: "How do you link JavaScript to HTML?",
+    choices: ["<link>", "<connect>", "<script>", "<src='javascript' />"],
+    answer: "<script>",
+  },
+  {
+    title: "How do we alert Correct?",
+    choices: [
+      "alertbox('Correct');",
+      "prompt('Correct')",
+      "alert('Correct')",
+      "alert{correct}",
+    ],
+    answer: "alert('Correct')",
+  },
+  {
+    title: "JavaScript is a ________ Side Scripting Language.",
+    choices: ["Server", "Browser", "Right", "API"],
+    answer: "Browser",
+  },
+  {
+    title: "Which was the first browser to support JavaScript?",
+    choices: ["Netscape", "Internet Explorer", "Safari", "Google Chrome"],
+    answer: "Netscape",
+  },
+  {
+    title: "How do you print a value to the Console?",
+    choices: [
+      "print(value)",
+      "write(value)",
+      "console.print(value)",
+      "console.log(value)",
+    ],
+    answer: "console.log(value)",
+  },
+];
